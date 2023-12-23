1b31f9f23ddc0b06f10e9299b3ede2ae53423cbf
curioquest <agifford101.dev@gmail.com>
2023-12-23 12:36:54 -0800

    tweaking cors origins

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=[
-            "https://oyster-app-cxtg3.ondigitalocean.app:8080",
+            # "https://oyster-app-cxtg3.ondigitalocean.app:8080",
             # "https://oyster-app-cxtg3.ondigitalocean.app:8080/",
             "https://oyster-app-cxtg3.ondigitalocean.app",
             # "https://oyster-app-cxtg3.ondigitalocean.app/"
