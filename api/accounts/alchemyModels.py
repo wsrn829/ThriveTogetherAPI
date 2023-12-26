ef497db7e3e3f2903692d73d90a211faecea8469
curioquest <agifford101.dev@gmail.com>
2023-12-26 11:42:20 -0800

    moved users models table to accounts folder

--- api/accounts/alchemyModels.py
+++ api/accounts/alchemyModels.py
@@ -1,18 +1,22 @@
 from sqlalchemy import Column, Integer, String
 from base import Base
 
+# Assuming Base is your declarative base instance in SQLAlchemy
 
-class Accounts(Base):
-    __tablename__ = 'accounts'
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
-    name = Column(String)
-    age = Column(Integer)
-    gender = Column(String)
-    pronouns = Column(String)
-    email = Column(String)
+class Users(Base):
+    __tablename__ = 'users'
 
-    def __repr__(self):
-        return f"<Account(username='{self.username}', email='{self.email}')>"
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    hashed_password = Column(String(1000), nullable=False)
+    name = Column(String(100), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(50), nullable=False)
+    pronouns = Column(String(10), nullable=False)
+    profile_link = Column(String(75))
+    profile_image = Column(String(1000))
+    banner_image = Column(String(1000))
+    email = Column(String(150), nullable=False)
+    about_me = Column(String(5000))
+    my_story = Column(String(5000))
